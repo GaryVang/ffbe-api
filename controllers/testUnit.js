83f2a9c05ec47dfdf652a0135210141b0aad6ed7
MysticV <leng_vang89@hotmail.com>
2019-10-25 01:16:41 -0700

    First commit. Change static sql queries later.

---
+++ controllers/testUnit.js
@@ -0,0 +1,19 @@
+const handleTestUnit = (db) => (req, res) => {
+    // const { unit_id } = req.body;
+
+    // return db.select('*').from('stats').where('unit_id', '=', unit_id )
+   
+    // currently static, fix later
+    return db.select('*').from('stats')
+    .join('units', function() {
+        this.on('units.unit_id', '=', 'stats.unit_id')
+    })
+    .then(unit => {
+        res.json(unit[0])
+    })
+    .catch(err => res.status(400).json('unable to get user'))
+}
+
+module.exports = {
+    handleTestUnit
+}
---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const knex = require('knex');
+
+const unit = require('./controllers/unit');
+const testUnit = require('./controllers/testUnit');
+
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'test123',
+      database : 'ffbe'
+    }
+});
+
+db.select('*').from('units')
+    .then(data => { console.log(data)});
+
+const app = express();
+
+// const database = {
+//     units: [
+//         {
+//             name: 'Lightning'
+//         }
+//     ]
+// }
+
+app.unsubscribe(bodyParser.json());
+app.use(cors());
+
+// app.get('/', (req, res) => { res.send(db.units)  })
+app.get('/', (req, res) => { res.send(db.select('*').from('units'))  })
+
+// app.get('/', (req, res) => { return db.select('*').from('units') })
+
+// app.get('/', (req, res) => { res.send(db.select('*').from('units')
+//                                     .then(unit => {
+//                                         res.json(unit[0])
+//                                     })
+//                      .catch(err => res.status(400).json('unable to get user')) ) 
+//                                 })
+
+// app.get('/unit/:id', (req, res, db) => { unit.handleUnitGet(req, res, db) })
+
+app.get('/testUnit', testUnit.handleTestUnit(db))
+
+
+
+app.listen(3000, ()=> {
+    console.log('app is running on port 3000');
+})
