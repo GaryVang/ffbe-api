4fb17d4f875742e9e72f069e2b31d16c88fefa99
MysticV <leng_vang89@hotmail.com>
2019-11-27 02:59:41 -0800

    Now correctly returns equipment in json format. Also added error checking to testLoadEq

--- controllers/testLoadEq.js
+++ controllers/testLoadEq.js
@@ -1,10 +1,11 @@
 const handleTestLoadEq = (db) => (req, res) => {
     return db.select('*')
         .from('test_equipment')
-        .whereRaw('(info ->> ?) = ?', ['name', 'omega weapon'])
-    .then(equipment => {
-        equipment[0].info
-    })
+        .whereRaw('(info ->> ?) = ?', ['name', 'storm kickers'])
+        .then(equipment => {
+            res.json(equipment[0].info)
+        })
+    .catch(err => res.status(400).json('unable to retrieve equipment'))
 }
 
 module.exports = {
