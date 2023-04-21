3a2e1427e0d7b911b98da524df88fe133fb56bbc
Gary Vang <leng_vang89@hotmail.com>
2023-04-21 00:45:49 -0700

    Update server.js
    knex config changes based on research. Supposedly there are issues using knex + psql + Render.

--- server.js
+++ server.js
@@ -11,7 +11,18 @@ const db = knex({
   connection: {
     connectionString : process.env.DATABASE_URL,
     ssl: true,
-  }
+  },
+pool: {
+      min: 0,
+      max: 7,
+      acquireTimeoutMillis: 300000,
+      createTimeoutMillis: 300000,
+      destroyTimeoutMillis: 50000,
+      idleTimeoutMillis: 300000,
+      reapIntervalMillis: 10000,
+      createRetryIntervalMillis: 2000,
+      propagateCreateError: false,
+    }
 });
 
 const app = express();
